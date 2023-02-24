23c9ddb426cf84d805ef74b41ddedb5d9f336ecb
kevin <68902447+kevinJ045@users.noreply.github.com>
2023-02-24 02:59:51 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var fs = require('fs');
+var path = require('path');
+var sqlite3 = require('sqlite3');
+var app = express();
+
+var db = new sqlite3.Database('attendance.db');
+db.serialize(function() {
+
+  db.run("CREATE TABLE IF NOT EXISTS attend (id INTEGER PRIMARY KEY, name TEXT, date TEXT)");
+
+  app.post('/attend', (req, res) => {
+  	var date = new Date().toLocaleDateString();
+  	db.get("SELECT id, name, date FROM attend WHERE date='"+date+"' and name='"+req.query.name.toLowerCase()+"'", function(err, row) {
+    	res.send(!row ? db.run("INSERT INTO attend (name, date) VALUES ('"+ req.query.name.toLowerCase() +"', '"+date+"')") : 'false');
+  	});
+  });
+
+  app.get('/attended', (req, res) => {
+  	var date = new Date().toLocaleDateString();
+  	db.get("SELECT id, name, date FROM attend WHERE date='"+date+"' and name='"+req.query.name.toLowerCase()+"'", function(err, row) {
+  		console.log(err, row);
+    	res.send(!row ? 'false' : 'true');
+  	});
+  });
+
+  app.get('/attendance', (req, res) => {
+  	db.all("SELECT id, name, date FROM attend", function(err, row) {
+  		console.log(err, row);
+    	res.send(row);
+  	});
+  });
+
+  app.get('/attendance/today', (req, res) => {
+  	var date = new Date().toLocaleDateString();
+  	db.all("SELECT id, name, date FROM attend WHERE date='"+date+"'", function(err, row) {
+  		console.log(err, row);
+    	res.send(row);
+  	});
+  });
+
+
+	app.use('/app', express.static(path.join(__dirname, 'app')));
+
+	app.listen(81, () => console.log('port at 81'));
+});
